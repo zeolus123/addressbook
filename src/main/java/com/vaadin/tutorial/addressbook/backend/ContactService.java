530054325ee71b67887f24ae3b35fa0a147ebf98
Zachery <zc321357@dal.ca>
2017-02-01 20:59:26 -0400

    Edited the address book to be a todo list

--- src/main/java/com/vaadin/tutorial/addressbook/backend/ContactService.java
+++ src/main/java/com/vaadin/tutorial/addressbook/backend/ContactService.java
@@ -38,12 +38,12 @@ public class ContactService {
                 Contact contact = new Contact();
                 contact.setFirstName(fnames[r.nextInt(fnames.length)]);
                 contact.setLastName(lnames[r.nextInt(fnames.length)]);
-                contact.setEmail(contact.getFirstName().toLowerCase() + "@"
-                        + contact.getLastName().toLowerCase() + ".com");
-                contact.setPhone("+ 358 555 " + (100 + r.nextInt(900)));
-                cal.set(1930 + r.nextInt(70),
-                        r.nextInt(11), r.nextInt(28));
-                contact.setBirthDate(cal.getTime());
+                contact.setTask("Finish " + contact.getFirstName() +"'s project iteration");
+          
+                cal.set(1930 + r.nextInt(70),r.nextInt(11), r.nextInt(28));
+                contact.setStartDate(cal.getTime());
+                cal.set(1930 + r.nextInt(70),r.nextInt(11), r.nextInt(28));
+                contact.setEndDate(cal.getTime());
                 contactService.save(contact);
             }
             instance = contactService;
